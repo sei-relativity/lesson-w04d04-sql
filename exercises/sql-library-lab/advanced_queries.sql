b5b377bf36a4756b19d5f97bef8308603a8baa57
Saud Alshamsi <23ud92@gmail.com>
2019-11-14 12:01:28 +0300

    DONE

--- exercises/sql-library-lab/advanced_queries.sql
+++ exercises/sql-library-lab/advanced_queries.sql
@@ -2,21 +2,22 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+SELECT * FROM books JOIN authors ON books.author_id = authors.id WHERE authors.name = 'George R.R. Martin';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+SELECT * FROM books JOIN authors ON books.author_id = authors.id WHERE authors.name = 'Milan Kundera'
 -- Find all books written by an author from China or the UK.
-
+SELECT title FROM books JOIN authors ON books.author_id = authors.id WHERE authors.nationality = 'China' OR authors.nationality = 'United Kingdom';
 -- Find out how many books Albert Camus wrote.
-
+SELECT books.title, authors.name from books JOIN authors ON books.author_id = authors.id WHERE authors.name = 'Albert Camus';
 -- Find out how many books were written by US authors.
-
+SELECT books.title, authors.name, authors.nationality from books JOIN authors ON books.author_id = authors.id WHERE authors.nationality = 'United States of America';
 -- Find all books written after 1930 by authors from Argentina.
-
+SELECT books.title, authors.name, authors.nationality, books.publication_date from books JOIN authors ON books.author_id = authors.id WHERE books.publication_date > 1930 AND authors.nationality = 'Argentina';
 -- Find all books written before 1980 by authors not from the US.
-
+SELECT books.title, authors.name, authors.nationality, books.publication_date from books JOIN authors ON books.author_id = authors.id WHERE books.publication_date < 1980 AND NOT authors.nationality = 'United States of America';
 -- Find all authors whose names start with 'J'.
-
+SELECT * FROM authors WHERE authors.name LIKE 'J%'
 -- Find all books whose titles contain 'the'.
-
+SELECT * FROM books WHERE title LIKE '%the%';
 -- Find all authors who have written books with that start with the letter 'N'.
+SELECT authors.name, books.title FROM books JOIN authors ON books.author_id = authors.id WHERE books.title LIKE 'N%';
