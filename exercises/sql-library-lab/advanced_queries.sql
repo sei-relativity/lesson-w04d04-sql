9bfffb1f7e424c2000c0e683b6792856db814a28
aba20 <askr1000@gmail.com>
2019-11-14 07:02:55 +0300

    HW: SQL

--- exercises/sql-library-lab/advanced_queries.sql
+++ exercises/sql-library-lab/advanced_queries.sql
@@ -2,21 +2,25 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+SELECT * FROM books JOIN authors ON books.author_id=authors.id WHERE authors.name= 'George R.R. Martin';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+SELECT * FROM books JOIN authors ON books.author_id=authors.id WHERE authors.name= 'Milan Kundera';
 -- Find all books written by an author from China or the UK.
+SELECT * FROM books JOIN authors ON authors.nationality=nationality WHERE authors.nationality= 'China' OR authors.nationality='Uited Kingdom';
 
 -- Find out how many books Albert Camus wrote.
-
+SELECT COUNT(author_id) FROM books JOIN authors ON books.author_id=authors.id WHERE authors.name='Albert Camus';
 -- Find out how many books were written by US authors.
-
+SELECT COUNT(author_id) FROM books JOIN authors ON books.author_id=authors.id WHERE authors.nationality='United States of America';
 -- Find all books written after 1930 by authors from Argentina.
-
+SELECT COUNT(author_id) FROM books JOIN authors ON books.author_id=authors.id WHERE authors.nationality='Argentina' AND books.publication_date > 1930;
 -- Find all books written before 1980 by authors not from the US.
+SELECT COUNT(author_id) FROM books JOIN authors ON books.author_id=authors.id WHERE authors.nationality!='United States of America' AND books.publication_date < 1980;
 
 -- Find all authors whose names start with 'J'.
-
+SELECT * FROM authors WHERE name LIKE 'J%';
 -- Find all books whose titles contain 'the'.
-
+SELECT * FROM books WHERE title LIKE '%the%';
 -- Find all authors who have written books with that start with the letter 'N'.
+
+SELECT * FROM authors JOIN books ON authors.name=name WHERE books.title LIKE 'N%';
