05829f89d5c27e7a1b3dc0f00e4d47e7df959d97
dalia <dalia.91a@gmail.com>
2019-11-14 10:38:04 +0300

    hw

--- exercises/sql-library-lab/basic_queries.sql
+++ exercises/sql-library-lab/basic_queries.sql
@@ -5,28 +5,31 @@
 -- SELECT * FROM authors;
 
 -- Get just the name and birth year of all authors
-
+SELECT name , birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
-
+SELECT name, birth_year FROM authors  WHERE birth_year > 1900;
 -- Get all authors born in the USA
-
+ SELECT name FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+SELECT title FROM books WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+SELECT title FROM books WHERE publication_date < 1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+ SELECT publication_date FROM books WHERE title='A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT *FROM books WHERE title LIKE  '%the%';
 -- Add yourself as an author
-
+ INSERT INTO authors (name , nationality, birth_year) VALUES ('Dalia', 'SA', 1991);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
+INSERT INTO books (title, publication_date, author_id) VALUES ('WHAT YOU WANT', 2019, 9);
+INSERT INTO books (title, publication_date, author_id) VALUES ('NULL', 2018, 9);
 -- Update one of your books to have a new title
-
+UPDATE books SET title='HI ALL' WHERE title='WHAT YOU WANT';
 -- Delete your books
-
+DELETE FROM books WHERE id = 69;
+DELETE FROM books WHERE id = 70;
 -- Delete your author entry
+DELETE FROM authors WHERE id = 9;
